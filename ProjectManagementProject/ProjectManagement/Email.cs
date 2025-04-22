2793b508976241ac425ab6fe1e06272a11eb00a8
Enrique_V <112908849+Ejvargas24@users.noreply.github.com>
2025-04-22 23:09:34 +0000

    refactored features

---
+++ ProjectManagementProject/ProjectManagement/Email.cs
@@ -0,0 +1,98 @@
+namespace ProjectManagement;
+using System;
+using System.IO;
+ 
+ public class Email{
+    string emailAnswer;
+    string emailWrite;
+    string labelAnswer;
+    List<string> WebList = new List<string>();
+    List<string> RevisionsList = new List<string>();
+    List<string> InvoicesList = new List<string>();
+    public Email(){
+    }
+
+    public void Display(){
+        Console.WriteLine("Please choose an option:");
+        Console.WriteLine("1. Add Email");
+        Console.WriteLine("2. View each email in categories");
+        Console.WriteLine("3. Back");
+    }
+    public string emailOption(){
+        emailAnswer = Console.ReadLine();
+        return emailAnswer;
+    }
+
+    public void labelEmail(){
+        Console.WriteLine("Please choose a category for the email:");
+        Console.WriteLine("1. Web Design");
+        Console.WriteLine("2. Invoices");
+        Console.WriteLine("3. Revisions");
+        labelAnswer = Console.ReadLine();
+        if (labelAnswer == "1"){
+            Console.WriteLine("Add the email here:");
+            emailWrite = Console.ReadLine();
+            using (StreamWriter file = new StreamWriter("WebCat.txt",true)){
+                file.WriteLine(emailWrite);
+            }
+        }
+        if (labelAnswer == "2"){
+            Console.WriteLine("Add the email here:");
+            emailWrite = Console.ReadLine();
+            using (StreamWriter file = new StreamWriter("InvCat.txt",true)){
+            file.WriteLine(emailWrite);
+            }
+        }
+         if (labelAnswer == "3"){
+            Console.WriteLine("Add the email here:");
+            emailWrite = Console.ReadLine();
+            using (StreamWriter file = new StreamWriter("RevCat.txt",true)){
+            file.WriteLine(emailWrite);
+            }
+        }
+
+    }
+
+    public void emailCategories(){
+        Console.WriteLine("The emails in each category are:");
+        Console.WriteLine("------------------------------");
+        Console.WriteLine("Web Design:");
+        try{
+            string webCatEmails = File.ReadAllText("WebCat.txt");
+            string[] result = webCatEmails.Split("\n");
+            Console.WriteLine("------------------------------");
+            foreach(var element in result) {
+            Console.WriteLine(element);
+         }
+         Console.WriteLine("------------------------------");
+
+        }catch(FileNotFoundException e){
+            Console.WriteLine("There are no emails in this category");
+        }
+        Console.WriteLine("Invoices:");
+        try{
+            string invCatEmails = File.ReadAllText("InvCat.txt");
+            string[] result = invCatEmails.Split("\n");
+            Console.WriteLine("------------------------------");
+            foreach(var element in result) {
+            Console.WriteLine(element);
+         }
+         Console.WriteLine("------------------------------");
+        }catch(FileNotFoundException e){
+            Console.WriteLine("There are no emails in this category");
+        }
+        Console.WriteLine("Revisions:");
+        try{
+            string revCatEmails = File.ReadAllText("RevCat.txt");
+            string[] result = revCatEmails.Split("\n");
+            Console.WriteLine("------------------------------");
+            foreach(var element in result) {
+            Console.WriteLine(element);
+         }
+        }catch(FileNotFoundException e){
+            Console.WriteLine("There are no emails in this category");
+        }
+        //Console.WriteLine("------------------------------");
+    }
+
+ }
---
+++ ProjectManagementProject/ProjectManagement/ToDo.cs
@@ -0,0 +1,68 @@
+namespace ProjectManagement;
+using System;
+using System.IO;
+
+public class ToDo{
+
+    string newTask;
+    string contentsOfList;
+    string taskToRemove;
+    string ToDoAnswer;
+
+
+
+    public ToDo(){
+
+    }
+
+
+    public void ToDoDisplay(){
+        Console.WriteLine("Please choose an option:");
+        Console.WriteLine("1. Add a task:");
+        Console.WriteLine("2. Remove a task:");
+        Console.WriteLine("3. View list");
+        Console.WriteLine("4. Back");
+    }
+
+    public string ToDoOption(){
+        ToDoAnswer = Console.ReadLine();
+        return ToDoAnswer;
+    }
+    public void ToDoAdd(){
+        Console.WriteLine("Task to add:");
+        newTask = Console.ReadLine();
+        File.AppendAllText("ToDo.txt",","+newTask);
+    }
+
+    public void ToDoRemove(){
+
+        contentsOfList = File.ReadAllText("ToDo.txt");
+        string[] result = contentsOfList.Split(",");
+        foreach(var element in result) {
+            Console.WriteLine(element);
+        }
+        List<string> listVersion = result.ToList();
+        Console.WriteLine("Write which task to remove:");
+        taskToRemove = Console.ReadLine();
+        listVersion.Remove(taskToRemove);
+        File.Delete("ToDo.txt");
+        string concatenateRemove = String.Join(",", listVersion);
+        Console.WriteLine("The new list of tasks:");
+        Console.WriteLine(concatenateRemove);
+        File.WriteAllText("ToDo.txt",concatenateRemove);
+    }
+
+    public void ToDoView(){
+        try{
+            contentsOfList = File.ReadAllText("ToDo.txt");
+            string[] result = contentsOfList.Split(",");
+            foreach(var element in result) {
+                Console.WriteLine(element);
+            }
+        }catch(FileNotFoundException e){
+            Console.WriteLine("The To-Do list is currently empty");
+        }
+        
+    }
+
+}
